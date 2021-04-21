0343a46236959a2ffd7fd2e1d395f29b8e9e1019
kraimaim <67071210+kraimaim@users.noreply.github.com>
2021-04-21 18:27:38 -0300

    filter added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inputFilter = document.querySelector('#filter')
 const buttonSearch = document.querySelector('#search')
 
 
+
 let firstCall = true;
 
 
@@ -60,27 +61,54 @@ async function startPost() {
         if((100/limite) >=pagina){
             pagina = pagina +1;
         }else {pagina = Math.floor(Math.random()*5)+1;
-            limite = Math.floor(Math.random()*5)+1
+            limite = Math.floor(Math.random()*5)+1;
         }
         
 }
 
-window.onscroll = () => {
-    let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;  if (bottomOfWindow) {
-       if(firstCall === false) { startLoader();
-        
-      setTimeout(function() {
-        finishLoader()
-        startPost();},1500)
-    }else{console.log('tenes que tocar el boton por lo menos una vez!');}
+inputFilter.addEventListener('input', (e) => {
+    // Recibir lo que estoy escribiendo
+    const incomingData = e.target.value.toUpperCase();
+
+    // Recibir todas las tarjetas de los posts
+    const allPosts = document.querySelectorAll('.post');
+    console.log(allPosts);
+    // Recorrer los posts que recibi anteriormente, y mostrar u ocultar algunos dependiendo de lo que haya escrito
+
+    for (const post of allPosts) {
+        const title = post.querySelector('.post-title').innerText.toUpperCase();
+        const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+        if (title.includes(incomingData) || body.includes(incomingData)) {
+            post.style.display = 'flex';
+        }else {
+            post.style.display = 'none';
+        }
     }
-  };
+});
+
+window.onscroll = () => {
+    let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;  if (bottomOfWindow && firstCall === false && inputFilter.value.length == 0) {
+        inputFilter.value.length == 0
+            startLoader();
+            setTimeout(function() {
+                finishLoader()
+                startPost();},1500)
+        
+        }
+  }
 
 
 
+//creo que me equivoque
 // TAREA
 // Pasos a seguir 
 // 1- quiero que detecte cuando llego a abajo de todo
 // 2- cuando llego abajo de todo, quiero que muestre el loading
 // 3- despues, que me cargue otros 5 post y los agregue
 // 3-a-cuando llego al final, me cambie dinamicamente las variables pagina y limite.
+// 4- Hacer el filter para filtrar
+// 5) T O D O: Filtrar por titulo y por el body del post
+// 6) T O D O: Que cuando este filtrado y llegue al final, no siga cargando posts.
+// 7) T O D O: Les voy a pasar un string y tienen que contar cuantas letras Ftiene ese string.
+// El string es el siguiente: 'Fuimos a Francia fuertemente falopeados comiendo fajitas de jamon y nos fajaron fuerte
